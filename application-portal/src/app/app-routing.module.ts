283115221d35bc08078f1d261770841c03de78ea
Lucy Fan <lucyfan@Lucys-MacBook-Air.local>
2019-07-25 10:33:35 +0100

    create html and css for courses and course-info to match design

--- application-portal/src/app/app-routing.module.ts
+++ application-portal/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
-  { path: 'fields', loadChildren: './fields/fields.module#FieldsPageModule' },
-  { path: 'courses', loadChildren: './courses/courses.module#CoursesPageModule' },
-  { path: 'course-info', loadChildren: './course-info/course-info.module#CourseInfoPageModule' },
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  { path: 'fields', loadChildren: './pages/fields/fields.module#FieldsPageModule' },
+  { path: 'courses', loadChildren: './pages/courses/courses.module#CoursesPageModule' },
+  { path: 'course-info', loadChildren: './pages/course-info/course-info.module#CourseInfoPageModule' },
 ];
 
 @NgModule({
--- application-portal/src/app/pages/courses/courses.page.ts
+++ application-portal/src/app/pages/courses/courses.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  buttonClick() {
+    console.log("button clicked");
+    this.navCtrl.navigateForward('course-info');
   }
 
 }
